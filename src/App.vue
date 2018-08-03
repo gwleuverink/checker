e6e3d8891b408428acbb1870a25f78d98596f331
Niels Voogt <niels@fitmanager.com>
2018-08-03 17:33:18 +0200

    Added views and basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
-    <transition name="slide">
-        <div class="">
-            x
-        </div>
-    </transition>
+  <div id="app">
+    <checker-viewer />
+  </div>
 </template>
 
 <script>
+
+import viewer from './components/viewer.vue';
+
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  components: {
+    'checker-viewer': viewer
+  },
+  // Method to set localstorage 'accepted_disclaimer' to true
+  // Look up if user localStorage has 'accepted_disclaimer', if true check for date/type else view is
 }
+
+
 </script>
 
 <style lang="scss">
+
     @import './scss/app.scss';
+
+  .view {
+      align-items: center;
+      background: pink;
+      bottom: 0;
+      display: flex;
+      flex-direction: column;
+      left: 0;
+      position: absolute;
+      right:0;
+      text-align: center;
+      top: 0;
+  }
 </style>
---
+++ src/views/disclaimer.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+
+        disclaimer hier
+
+        <button @click="$emit('navigateTo', 1)">
+            Ik snap het compleet
+        </button>
+    </div>
+</template>
